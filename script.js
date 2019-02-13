2864844f4d47a0f3a5496dc2e64262ac64816b26
Aquitan <suharevskiy@intaro.email>
2019-02-13 17:58:08 +0300

    lesson-2 commit

--- script.js
+++ script.js
@@ -24,6 +24,45 @@ for(let i = 0; i < excel.length; i++) {
     }
 }
 
+//Генерируем появление элементов змеи
 function generateSnake() {
+    let posX = Math.round(Math.random() * (10 - 3) + 3);
+    let posY = Math.round(Math.random() * (10 - 1) + 1);
+    return [posX, posY];
+}
+
+let coordinates = generateSnake();
+//Генерация ячеек змеи подряд через массив
+let snakeBody = [document.querySelector('[posX =  "' + coordinates[0] + '"][posY = "' + coordinates[1] + '"]'), document.querySelector('[posX =  "' + (coordinates[0] - 1) + '"][posY = "' + coordinates[1] + '"]'), document.querySelector('[posX =  "' + (coordinates[0] - 2) + '"][posY = "' + coordinates[1] + '"]')];
+
+//Присваиваем класс snakeBody для ячеек с телом змеии
+for (var i = 0; i < snakeBody.length; i++) {
+    snakeBody[i].classList.add('snake-body');
+}
+
+snakeBody[0].classList.add('snake-head');
+
+//Создаем мышь
+
+let mouse;
+
+function createMouse() {
+    function generateMouse() {
+        let posX = Math.round(Math.random() * (10 - 3) + 3);
+        let posY = Math.round(Math.random() * (10 - 1) + 1);
+        return [posX, posY];
+    }
+
+    let mouseCoordinates = generateMouse();
+        mouse = document.querySelector('[posX =  "' + mouseCoordinates[0] + '"][posY = "' + mouseCoordinates[1] + '"]');
+
+    while(mouse.classList.contains('snake-body')) {
+        let mouseCoordinates = generateMouse();
+        mouse = document.querySelector('[posX =  "' + mouseCoordinates[0] + '"][posY = "' + mouseCoordinates[1] + '"]');
+    }
+
+    mouse.classList.add('mouse');
 
 }
+
+createMouse();
